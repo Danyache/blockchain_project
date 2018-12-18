f228bb8b7e48a657211ac79766902b2b76ae10e6
danyache1812 <danyache1812@yandex.ru>
2018-12-18 13:43:15 +0300

    fixed 2.0

--- blockchain_project.py
+++ blockchain_project.py
@@ -153,13 +153,10 @@ async def process_help_command(message: types.Message):
     length = len(s)
     n = []
     a = []
-    await bot.send_message(message.from_user.id, text)
     for i in range(int(length/2)):
         n.append(int(s[i]))
         a.append(int(s[i+int(length/2)]))
 
-    await bot.send_message(message.from_user.id, str(n))
-    await bot.send_message(message.from_user.id, str(a))
     result = crt(n, a)
 
     await bot.send_message(message.from_user.id, result)
